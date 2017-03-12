5429e408936cf9166d0247d19ad1f36a799a051e
wiki <chenweikuncsu@163.com>
2017-03-12 22:51:50 +0800

    nothing special

--- Shapes/src/shapes/MyPic.java
+++ Shapes/src/shapes/MyPic.java
@@ -4,16 +4,18 @@ public class MyPic {
 	public static void main(String[] args) 
 	{
 		Picture pic = new Picture(420,300);
-		Circle c1 = new Circle(320,40,80);
+		Circle c1 = new Circle(320,40,20);
 		Rectangle r1 = new Rectangle(100, 100, 100, 100);
 		Triangle t1 = new Triangle(100, 100, 200, 100, 150, 50);
 		Line l1 = new Line(0,205,400,205);
 		Circle c2 = new Circle(200,200,50);
+		Line l2 = new Line(0,200,400,200);
 		pic.add(c1);
 		pic.add(r1);
 		pic.add(t1);
 		pic.add(l1);
 		pic.add(c2);
+		pic.add(l2);
 		pic.draw();	
 		int anar[]= new int[5];
 		System.out.println(anar[0]);
